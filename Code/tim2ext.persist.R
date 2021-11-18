dbe99e3b726f37a5041cbe9d3ccfd5b909adee8e
mgree013 <mgree013@ucr.edu>
2021-11-18 09:58:34 -0700

    new work upload

--- Code/tim2ext.persist.R
+++ Code/tim2ext.persist.R
@@ -148,36 +148,46 @@ loc.all%>%
   theme_bw()+ theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),panel.border = element_rect(colour = "black"))+
   theme(legend.position = "none")+facet_grid(~var,scales="free")
 
-
+###Trophic
 loc.all%>%
+ # filter(number.bottles !="8")%>%
   ggplot(aes(x=log(prey.den+1),y=pred.persistence))+
   geom_point()+geom_smooth(method = "lm")+
   scale_color_viridis(discrete = TRUE)+
   theme_bw()+ theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),panel.border = element_rect(colour = "black"))+
-  theme(legend.position = "none")#+facet_grid(as.factor(patch.degree)~prod)
+  theme(legend.position = "none")+facet_grid(as.factor(patch.degree)~prod)
 
 
 loc.all%>%
+  #filter(number.bottles !="8")%>%
   ggplot(aes(x=log(pred.den+1),y=prey.persistence))+
   geom_point()+geom_smooth(method = "lm")+
   scale_color_viridis(discrete = TRUE)+
   theme_bw()+ theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),panel.border = element_rect(colour = "black"))+
-  theme(legend.position = "none")#+facet_grid(as.factor(patch.degree)~prod)
+  theme(legend.position = "none")#+facet_grid(~prod)
+
+loc.all%>%
+  #filter(number.bottles !="8")%>%
+  ggplot(aes(x=prey.oc,y=pred.persistence))+
+  geom_point()+geom_smooth(method = "lm")+
+  scale_color_viridis(discrete = TRUE)+
+  theme_bw()+ theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),panel.border = element_rect(colour = "black"))+
+  theme(legend.position = "none")+facet_grid(prey~structure)
                                              
 reg.all.plot%>%
   filter(pred.ext=="no")%>%
-  ggplot(aes(x=prey.dens,y=pred.persistence))+
+  ggplot(aes(x=prey.oc,y=pred.persistence))+
   geom_point()+geom_smooth(method = "lm")+
   scale_color_viridis(discrete = TRUE)+
   theme_bw()+ theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),panel.border = element_rect(colour = "black"))+
   theme(legend.position = "none")#+facet_grid(as.factor(patch.degree)~prod)        
 
 reg.all.plot%>%
-  ggplot(aes(x=pred.dens,y=prey.persistence))+
+  ggplot(aes(x=pred.oc,y=prey.persistence))+
   geom_point()+geom_smooth(method = "lm")+
   scale_color_viridis(discrete = TRUE)+
   theme_bw()+ theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),panel.border = element_rect(colour = "black"))+
-  theme(legend.position = "none")#+facet_grid(as.factor(patch.degree)~prod)                                    
+  theme(legend.position = "none")+facet_grid(~prod)                                    
      
 ################################################################################################################################################
 #Proportion of Metacommunity Extinctions
@@ -185,27 +195,27 @@ reg.all.plot%>%
 ext.prop<-loc.all%>%
   group_by(predator,prey,productivity,log.network.syn.lap,log.number.bottles,replicate,structure,media,year,log.total.vol,nghbr.connect,number.bottles) %>%
   summarize(prop.prey.met.ext=sum(prey.ext.quant)/number.bottles,prop.pred.met.ext=sum(pred.ext.quant)/number.bottles,
-            prey.ext=if_else(prey.ext.quant>=1, "yes", "no"),pred.ext=if_else(pred.ext.quant>=1, "yes", "no"))
+            prey.ext=if_else(prop.prey.met.ext>=0, "yes", "no"),pred.ext=if_else(prop.prey.met.ext>=0, "yes", "no"))
   
 ext.prop%>%
-  #filter(number.bottles>1)%>%
+  #filter(prop.pred.met.ext>0)%>%
   gather(log.number.bottles,log.network.syn.lap,log.total.vol,nghbr.connect, key = "var", value = "value")%>%
   ggplot(aes(x=value,y=prop.pred.met.ext))+
   geom_point()+geom_smooth(method = "lm")+
   scale_color_viridis(discrete = TRUE)+
   ylab("Proportion of Predator Metacommunites Extinct")+
   theme_bw()+ theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),panel.border = element_rect(colour = "black"))+
-  theme(legend.position = "none")+facet_grid(prey.ext~var,scales="free")          
+  theme(legend.position = "none")+facet_grid(pred.ext~var,scales="free")          
 
 ext.prop%>%
-  filter(number.bottles>1)%>%
+ # filter(prop.prey.met.ext>0)%>%
   gather(log.number.bottles,log.network.syn.lap,log.total.vol,nghbr.connect, key = "var", value = "value")%>%
   ggplot(aes(x=value,y=prop.prey.met.ext))+
   geom_point()+geom_smooth(method = "lm")+
   scale_color_viridis(discrete = TRUE)+
   ylab("Proportion of Prey Metacommunites Extinct")+
   theme_bw()+ theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),panel.border = element_rect(colour = "black"))+
-  theme(legend.position = "none")+facet_grid(~var,scales="free")     
+  theme(legend.position = "none")+facet_grid(prey.ext~var,scales="free")     
 
 ext.prop%>%
   ggplot(aes(x=as.factor(productivity),y=prop.prey.met.ext, fill=as.factor(productivity)))+
@@ -222,3 +232,4 @@ ext.prop%>%
   ylab("Proportion of Predator Metacommunites Extinct")+xlab("Productivity")+
   theme_bw()+ theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),panel.border = element_rect(colour = "black"))+
   theme(legend.position = "none")
+
