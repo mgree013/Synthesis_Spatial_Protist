51bdf9b01d0511d547017d5f81ac37341c42d86b
mgree013 <mgree013@ucr.edu>
2022-03-11 16:40:36 -0700

    coefficetns and model cleaned

--- Code/ext_col_analysis.R
+++ Code/ext_col_analysis.R
@@ -298,6 +298,7 @@ reported.table2
 
 plot_model(mod14)
 check_collinearity(mod14)
+summary(mod14)
 
 pseudoR0 <- ((mod0$null.deviance-mod0$deviance)/mod0$null.deviance)
 pseudoR1 <- ((mod1$null.deviance-mod1$deviance)/mod1$null.deviance)
@@ -349,6 +350,7 @@ reported.table2
 
 plot_model(mod14)
 check_collinearity(mod14)
+summary(mod14)
 
 pseudoR0 <- ((mod0$null.deviance-mod0$deviance)/mod0$null.deviance)
 pseudoR1 <- ((mod1$null.deviance-mod1$deviance)/mod1$null.deviance)
@@ -400,6 +402,7 @@ reported.table2
 
 plot_model(mod14)
 check_collinearity(mod14)
+summary(mod14)
 
 pseudoR0 <- ((mod0$null.deviance-mod0$deviance)/mod0$null.deviance)
 pseudoR1 <- ((mod1$null.deviance-mod1$deviance)/mod1$null.deviance)
@@ -451,6 +454,7 @@ reported.table2
 
 plot_model(mod14)
 check_collinearity(mod14)
+summary(mod14)
 
 pseudoR0 <- ((mod0$null.deviance-mod0$deviance)/mod0$null.deviance)
 pseudoR1 <- ((mod1$null.deviance-mod1$deviance)/mod1$null.deviance)
