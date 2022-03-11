1e9bbe8fce75ee9b6071b2d155dee022a906bf0f
mgree013 <mgree013@ucr.edu>
2022-03-11 14:19:33 -0700

    extract coefficents

--- Code/tim2ext.analysis.R
+++ Code/tim2ext.analysis.R
@@ -193,6 +193,7 @@ reported.table2
 
 plot_model(mod14)
 check_collinearity(mod14)
+summary(mod14)
 
 pseudoR0 <- ((mod0$null.deviance-mod0$deviance)/mod0$null.deviance)
 pseudoR1 <- ((mod1$null.deviance-mod1$deviance)/mod1$null.deviance)
@@ -247,6 +248,7 @@ reported.table2
 
 plot_model(mod14)
 check_collinearity(mod14)
+summary(mod14)
 
 pseudoR0 <- ((mod0$null.deviance-mod0$deviance)/mod0$null.deviance)
 pseudoR1 <- ((mod1$null.deviance-mod1$deviance)/mod1$null.deviance)
