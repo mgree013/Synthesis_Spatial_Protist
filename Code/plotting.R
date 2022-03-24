cf8974ffbb4974dc6edc48a3ef37b7ae59887dc0
mgree013 <mgree013@ucr.edu>
2022-03-24 09:31:15 -0600

    cleaned

--- Code/plotting.R
+++ Code/plotting.R
@@ -1,26 +1,5 @@
 #plotting predicted values from best fit models~
 ##################################################################################################################################
-####
-## example from Venables and Ripley (2002, pp. 190-2.)
-ldose <- rep(0:5, 2)
-numdead <- c(1, 4, 9, 13, 18, 20, 0, 2, 6, 10, 12, 16)
-sex <- factor(rep(c("M", "F"), c(6, 6)))
-SF <- cbind(numdead, numalive = 20-numdead)
-budworm.lg <- glm(SF ~ sex*ldose, family = binomial)
-summary(budworm.lg)
-
-plot(c(1,32), c(0,1), type = "n", xlab = "dose",
-     ylab = "prob", log = "x")
-text(2^ldose, numdead/20, as.character(sex))
-ld <- seq(0, 5, 0.1)
-lines(2^ld, predict(budworm.lg, data.frame(ldose = ld,
-                                           sex = factor(rep("M", length(ld)), levels = levels(sex))),
-                    type = "response"))
-lines(2^ld, predict(budworm.lg, data.frame(ldose = ld,
-                                           sex = factor(rep("F", length(ld)), levels = levels(sex))),
-                    type = "response"))
-
-
 #Problem: It seems like its plotting same slope for all predictors. Need to switch it for specific slope of predictors
 
 #Part 1: Time to Extinction
