af49f3317556a6069f044571e06a284b0c875f2e
mgree013 <mgree013@ucr.edu>
2022-09-05 12:25:23 -0600

    new analytical framework explore

---
+++ Code/BRMS_Hierarchical_explore.R
@@ -0,0 +1,32 @@
+install.packages("brms")
+library(brms)
+
+##### S model #######
+S_m_alpha_4stu <- brm(S ~ Treatment + (Treatment | Study) +
+                        (1 | site.id / Block),
+                      family = 'poisson', #Poisson because S are integer values at alpha-scale
+                      data = alpha_data_4stu,
+                      cores = 4, chains = 4,
+                      iter = 4000,
+                      prior = c(
+                        prior('normal(0,1)', class = 'sd'),
+                        prior('normal(0,1)', class = 'b')),
+                      control = list(adapt_delta = 0.99, max_treedepth = 12))
+fixef(S_m_alpha_4stu)
+
+#
+y<-loc.all$prey.time.2.ext
+mod14<-glm(y~,family=poisson(link="log"),data=loc.all)
+###Time to Ext prey analysis
+
+S_m_alpha_4stu <- brm(y ~ as.factor(productivity)+log.number.bottles+nghbr.connect+as.factor(predator) +
+                        (Treatment | Study) + (1 | site.id / Block),
+                      family = 'poisson', #Poisson because S are integer values at alpha-scale
+                      data = alpha_data_4stu,
+                      cores = 4, chains = 4,
+                      iter = 4000,
+                      prior = c(
+                        prior('normal(0,1)', class = 'sd'),
+                        prior('normal(0,1)', class = 'b')),
+                      control = list(adapt_delta = 0.99, max_treedepth = 12))
+fixef(S_m_alpha_4stu)s
